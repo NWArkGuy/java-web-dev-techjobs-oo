cd76340e850707a5bfff146ddaeb7f84ef6b3788
William Crapps <william.crapps@gmail.com>
2021-02-11 16:45:54 -0600

    Step 2 Location and CoreCompetency Completed

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -17,7 +17,7 @@ public class CoreCompetency {
         this.value = value;
     }
 
-    // Custom toString, equals, and hashCode methods:
+// Custom toString, equals, and hashCode methods:
 
     @Override
     public String toString() {
@@ -39,4 +39,17 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
